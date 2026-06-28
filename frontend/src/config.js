dec7e39f344523d51ea6d99502e2dd09c9871ecf
sakshirai145 <raisakshi643@gmail.com>
2026-06-28 22:15:19 +0530

    Fix frontend config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,7 @@
-export const DASHBOARD_URL = process.env.REACT_APP_DASHBOARD_URL || "/dashboard";
+export const DASHBOARD_URL =
+  process.env.REACT_APP_DASHBOARD_URL ||
+  "https://zerodha-dashboard-stt8.onrender.com";
 
-export const API_URL = process.env.REACT_APP_API_URL || "";
+export const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://zerodha-backend-ddil.onrender.com";
